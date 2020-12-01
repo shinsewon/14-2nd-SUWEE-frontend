c27c1931141a57a461aa610d28b5758506fdc87f
류지혜 <ryujihye@lyujihyeui-MacBook-Pro.local>
2020-12-01 20:06:20 +0900

    [류지혜] Modify : NavTop 최종 수정

--- src/Styles/GlobalStyle.js
+++ src/Styles/GlobalStyle.js
@@ -3,19 +3,46 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&family=Noto+Serif+KR:wght@200;300;400;500;600;700;900&display=swap');
-
   ${reset}
-
   *{
-        box-sizing:border-box;
+    box-sizing:border-box;
     }
-    body{
-        font-size:14px;
-        background-color:rgba(20,20,20,0.5);
-        box-sizing: border-box;
+  body{
+    font-size:14px;
+    box-sizing: border-box;
     font-family: 'Noto Sans KR', 'Noto Serif KR', 'sans-serif', 'serif';
     }
     a{
+    text-decoration:none;
+    color:inherit;
+    cursor: pointer;
+    }
+    ol, ul, li {
+    list-style: none;
+    }
+    img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    }
+    input, button {
+    background-color: transparent;
+    }
+    h1, h2, h3, h4, h5, h6 {
+    font-family:'Maven Pro', sans-serif;
+  }
+
+<<<<<<< HEAD
+
+
+
+
+=======
+  * {
+    box-sizing: border-box;
+  }
+  
+  a{
         text-decoration:none;
         color:inherit;
         cursor: pointer;
@@ -34,11 +61,7 @@ const GlobalStyle = createGlobalStyle`
     h1, h2, h3, h4, h5, h6 {
     font-family:'Maven Pro', sans-serif;
   }
-
-
-
-
-
+>>>>>>> 35c06f6... [류지혜] Add : NavTop UI 구현 완료
 `;
 
 export default GlobalStyle;
